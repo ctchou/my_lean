2ffedd0bb5af71de50e87792d2a3bbbd90409b67
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2024-12-18 16:25:45 -0800

    Work on MyLean/NonMeasurable.lean

--- MyLean/NonMeasurable.lean
+++ MyLean/NonMeasurable.lean
@@ -33,10 +33,6 @@ Set.image_diff.{u_1, u_2} {α : Type u_1} {β : Type u_2} {f : α → β} (hf : 
 -/
 #check Set.image_diff
 /-
-Real.volume_Icc {a b : ℝ} : volume (Icc a b) = ENNReal.ofReal (b - a)
--/
-#check volume_Icc
-/-
 MeasureTheory.measure_inter_add_diff.{u_1} {α : Type u_1} {m : MeasurableSpace α} {μ : Measure α} {t : Set α}
   (s : Set α) (ht : MeasurableSet t) : μ (s ∩ t) + μ (s \ t) = μ s
 -/
@@ -56,19 +52,11 @@ MeasureTheory.measure_diff'.{u_1} {α : Type u_1} {m : MeasurableSpace α} {μ :
   (hm : NullMeasurableSet t μ) (h_fin : μ t ≠ ⊤) : μ (s \ t) = μ (s ∪ t) - μ t
 -/
 #check MeasureTheory.measure_diff'
-/-
-MeasureTheory.measure_union'.{u_1} {α : Type u_1} {m : MeasurableSpace α} {μ : Measure α} {s₁ s₂ : Set α}
-  (hd : Disjoint s₁ s₂) (h : MeasurableSet s₁) : μ (s₁ ∪ s₂) = μ s₁ + μ s₂
--/
-#check MeasureTheory.measure_union'
-/-
-MeasureTheory.measure_biUnion_null_iff.{u_1, u_2, u_3} {α : Type u_1} {ι : Type u_2} {F : Type u_3}
-  [FunLike F (Set α) ENNReal] [OuterMeasureClass F α] {μ : F} {I : Set ι} (hI : I.Countable) {s : ι → Set α} :
-  μ (⋃ i ∈ I, s i) = 0 ↔ ∀ i ∈ I, μ (s i) = 0
--/
-#check MeasureTheory.measure_biUnion_null_iff
 
 /-- We first list the measure theoretic results that we need and specialize them to the measure `volume` on the reals. -/
+
+example {a b : ℝ} : volume (Icc a b) = ENNReal.ofReal (b - a) :=
+  volume_Icc
 
 lemma volume_mono {s t : Set ℝ} (h : s ⊆ t) : volume s ≤ volume t := by
   exact OuterMeasureClass.measure_mono volume h
@@ -79,6 +67,9 @@ lemma shift_volume (s : Set ℝ) (c : ℝ) : volume ((fun x ↦ x + c)''s) = vol
 lemma shift_measurable {s : Set ℝ} (h : MeasurableSet s) (c : ℝ) : MeasurableSet ((fun x ↦ x + c)''s) := by
   apply (MeasurableEmbedding.measurableSet_image ?_).mpr h
   exact measurableEmbedding_addRight c
+
+lemma union_volume {s t : Set ℝ} (hd : Disjoint s t) (h : MeasurableSet s) : volume (s ∪ t) = volume s + volume t :=
+    measure_union' hd h
 
 lemma biUnion_measurable {ι : Type*} {I : Set ι} {f : ι → Set ℝ}
     (hs : I.Countable) (hm : ∀ i ∈ I, MeasurableSet (f i)) : MeasurableSet (⋃ i ∈ I, f i) :=
@@ -103,14 +94,45 @@ lemma nullmeasurable_measurable {s : Set ℝ} (h : NullMeasurableSet s volume) :
   constructor
   . exact measure_congr t_eq_s
   . refine ae_le_set.mp ?_
-    exact EventuallyEq.le ((Filter.EventuallyEq.symm t_eq_s))
+    exact t_eq_s.symm.le
 
 /-- We also need some results about sets and functions. -/
 
-example {ι α : Type*} {I : Set ι} {s t : Set α} {f : ι → Set α → Set α}
-    (h : t ⊆ s) : ⋃ i ∈ I, f i s = (⋃ i ∈ I, f i t) ∪ (⋃ i ∈ I, f i (s \ t)) := by
-  sorry
+lemma biUnion_image_split {ι α : Type*} {I : Set ι} {s t : Set α} {f : ι → α → α}
+    (h : t ⊆ s) : ⋃ i ∈ I, (f i)''s = (⋃ i ∈ I, (f i)''t) ∪ (⋃ i ∈ I, (f i)''(s \ t)) := by
+  apply subset_antisymm
+  . intro a
+    rw [mem_union, mem_iUnion₂]
+    rintro ⟨i, i_I, a_fi⟩
+    rw [← union_diff_cancel h, image_union, mem_union] at a_fi
+    rcases a_fi with h_t | h_nt
+    . left ; rw [mem_iUnion₂] ; use i, i_I
+    . right ; rw [mem_iUnion₂] ; use i, i_I
+  . apply union_subset <;> apply biUnion_mono (subset_refl I) <;> intro i _
+    . exact image_mono h
+    . refine image_mono ?_
+      exact diff_subset
 
+lemma biUnion_image_disjoint {ι α : Type*} {I : Set ι} {s t : Set α} {f : ι → α → α}
+    (h : t ⊆ s) (hi : ∀ i ∈ I, (f i).Injective) (hd : I.PairwiseDisjoint (fun i ↦ (f i)''s)) :
+    Disjoint (⋃ i ∈ I, (f i)''t) (⋃ i ∈ I, (f i)''(s \ t)) := by
+  rw [Set.disjoint_left]
+  intro a
+  rw [mem_iUnion₂] ; rintro ⟨i, i_I, a_fi⟩
+  rw [mem_iUnion₂] ; rintro ⟨j, j_I, a_fj⟩
+  rcases eq_or_ne i j with h_eq | h_ne
+  . rw [mem_image] at a_fi a_fj
+    rcases a_fi with ⟨x, x_t, x_a⟩
+    rcases a_fj with ⟨y, y_nt, y_a⟩
+    have h_ij : f i x = f j y := by rw [x_a, y_a]
+    rw [← h_eq] at h_ij
+    have h_xy := hi i i_I h_ij
+    rw [← h_xy, mem_diff] at y_nt
+    tauto
+  . have a_fi_s : a ∈ (f i)''s := image_mono h a_fi
+    have a_fj_s : a ∈ (f j)''s := image_mono diff_subset a_fj
+    have := Set.disjoint_iff.mp (hd i_I j_I h_ne) (mem_inter a_fi_s a_fj_s)
+    exact not_mem_empty a this
 
 /-- In the setoid vS, two reals in the interval [0,1] are equivalent iff their difference is rational. -/
 

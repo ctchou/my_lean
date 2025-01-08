3942af62f224f2c4986dee698d1c0d96216ddf4d
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-01-08 15:52:15 -0800

    Work on MyLean/ProbLYM.lean

--- MyLean/ProbLYM.lean
+++ MyLean/ProbLYM.lean
@@ -1,4 +1,5 @@
 
+import Mathlib.Data.List.Perm.Basic
 import Mathlib.Algebra.BigOperators.Ring
 import Mathlib.Algebra.Field.Rat
 import Mathlib.Algebra.Order.Field.Basic
@@ -9,9 +10,16 @@ set_option diagnostics true
 
 open BigOperators
 
-section
+noncomputable section
 
-variable (α : Type*) [Fintype α] (𝓕 : Finset (Finset α))
+variable {α : Type*} [Fintype α]
+
+def canonList : List α := Finset.toList (Finset.univ)
+
+def containSet (s : Finset α) {l : List α} (h : List.Perm canonList l) : Prop :=
+  sorry
+
+variable (𝓕 : Finset (Finset α))
 
 theorem LYM_inequality (h𝓕 : IsAntichain (· ⊆ ·) (𝓕 : Set (Finset α))) :
     ∑ A in 𝓕, (1 / (Fintype.card α).choose (A.card)) ≤ 1 := by

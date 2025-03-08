7a1d8fea3540febd2aacf60186cf009851472ec7
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-03-07 18:37:26 -0800

    Create MyLean/MoreDioph.lean

---
+++ MyLean/MoreDioph.lean
@@ -0,0 +1,24 @@
+/-
+Copyright (c) 2025-present Ching-Tsun Chou All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Ching-Tsun Chou
+-/
+
+import Mathlib.NumberTheory.Dioph
+
+--set_option diagnostics true
+--set_option diagnostics.threshold 10
+
+/-!
+# More results about Diophantine sets and functions
+
+## References
+
+[Davis1973] Martin Davis, "Hilbert's Tenth Problem is Unsolvable"
+The American Mathematical Monthly, Vol. 80, No. 3 (Mar., 1973), pp. 233-269
+<https://www.jstor.org/stable/2318447>
+-/
+
+section
+
+end

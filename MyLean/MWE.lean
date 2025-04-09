bf98c52d7f6845acca08a688a335d1b0885fd1d2
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-04-09 12:02:09 -0700

    Work on MyLean/MWE.lean

---
+++ MyLean/MWE.lean
@@ -0,0 +1,29 @@
+
+        /-
+        case h.left.left
+        A : Type u_1
+        I : Type u_2
+        M : I → Automaton A
+        as : ℕ → A
+        ss : ℕ → (i : I) × Automaton.State A
+        h_next : ∀ (k : ℕ), ss (k + 1) ∈ Automaton.next (ss k) (as k)
+        i : I
+        s0 : Automaton.State A
+        h_s0_init : s0 ∈ Automaton.init
+        h_s0_ss : ⟨i, s0⟩ = ss 0
+        ss_i : ℕ → Automaton.State A
+        h_init : ⟨i, ss_i 0⟩ ∈ ⋃ i, Sigma.mk i '' Automaton.init
+        h_ss_i : ∀ (k : ℕ), ss k = ⟨i, ss_i k⟩
+        ⊢ ss_i 0 ∈ Automaton.init
+        -/
+
+import Mathlib.Data.Set.Card
+import Mathlib.Data.Sigma.Basic
+
+class C where
+  T : Type*
+  s : Set T
+
+example (I : Type*) (F : I → C) (i : I) (x : (F i).T)
+    (h : ⟨i, x⟩ ∈ ⋃ i, Sigma.mk i '' (F i).s) : x ∈ (F i).s := by
+  sorry

9cd0d2186a4d6acdd4e582ce58c5932b1d1fac45
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2025-05-28 11:52:29 -0700

    Work on MyLean/TaoDavis.lean

--- MyLean/TaoDavis.lean
+++ MyLean/TaoDavis.lean
@@ -6,7 +6,8 @@
 import mathlib
 
 example (a D : ℕ → ℝ) (h1 : Antitone a) (h2 : ∀ k, D k ≥ 0)
-    (h3 : ∀ k, a k ≤ D k - D (k + 1)) : a k ≤ D 0 / (k + 1) := by
+    (h3 : ∀ k, a k ≤ D k - D (k + 1)) : ∀ k, a k ≤ D 0 / (k + 1) := by
+  intro k
   suffices h4 : (k + 1) * a k ≤ D 0 - D (k + 1) by
     have h5 : (↑k + 1) * a k ≤ D 0 := by have := h2 (k + 1) ; bound
     have h6 : 0 < k + (1 : ℝ) := by bound
@@ -24,7 +25,8 @@ example (a D : ℕ → ℝ) (h1 : Antitone a) (h2 : ∀ k, D k ≥ 0)
 /- The following shorter version is due to Bjørn Kjos-Hanssen -/
 
 example (a D : ℕ → ℝ) (h₁ : Antitone a) (h₂ : ∀ k, D k ≥ 0)
-    (h₃ : ∀ k, a k ≤ D k - D (k + 1)) : a k ≤ D 0 / (k + 1) := by
+    (h₃ : ∀ k, a k ≤ D k - D (k + 1)) : ∀ k, a k ≤ D 0 / (k + 1) := by
+  intro k
   have : (k + 1) * a k ≤ D 0 - D (k + 1) := by
     induction' k with k h_ind <;> simp
     · exact h₃ 0

4622fb9ff7ed9ef5b732fb3bb2270172dde0482c
Ching-Tsun Chou <chingtsun.chou@gmail.com>
2026-01-04 20:26:38 -0800

    Play

--- MyLean/Play.lean
+++ MyLean/Play.lean
@@ -1,1 +1,12 @@
-def hello := "world"
+
+#eval 1 + 2 * 5
+
+#eval String.append "Hello, " "Lean!"
+
+#eval String.append "it is " (if 1 > 2 then "yes" else "no")
+
+def add1 (n : Nat) : Nat := n + 1
+
+#eval add1 7
+
+#check add1
